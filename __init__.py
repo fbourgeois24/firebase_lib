37a461d4221e2866c812dd66a660bdfd07108618
fbourgeoi <francois.bourgeois@aiesh.be>
2022-09-07 15:11:16 +0200

    Correction import

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ import os
 
 try:
 	# On importe tout
-	from firebase_lib import firebase_database
+	from firebase_lib.firebase_lib import firebase_database
 except ModuleNotFoundError:
 	# Si module non trouvé, on installe les dépendances
 	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
-	from firebase_lib import firebase_database
+	from firebase_lib.firebase_lib import firebase_database
