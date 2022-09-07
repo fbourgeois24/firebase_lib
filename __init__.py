26eb9ea0982174af738a8c461db44d8af2f1e1b4
fbourgeoi <francois.bourgeois@aiesh.be>
2022-09-07 14:53:24 +0200

    import du nécessaire uniquement + import auto des modules

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
-import os, sys
-directory = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(directory)
-from firebase_lib import *
+import os
+
+try:
+	# On importe tout
+	from firebase_lib import firebase_database
+except ModuleNotFoundError:
+	# Si module non trouvé, on installe les dépendances
+	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
+	from firebase_lib import firebase_database
